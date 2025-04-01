09b161d30c329290e09031733abce413c3319e68
Matilda Erenius <matildaerenius@hotmail.com>
2025-04-01 19:15:21 +0200

    initial commit

---
+++ backend/PastCast/src/main/java/com/example/pastcast/PastCastApplication.java
@@ -0,0 +1,13 @@
+package com.example.pastcast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PastCastApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PastCastApplication.class, args);
+    }
+
+}
---
+++ backend/PastCast/src/test/java/com/example/pastcast/PastCastApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pastcast;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PastCastApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
